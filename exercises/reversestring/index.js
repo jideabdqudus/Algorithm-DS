730a10fafeb1c1572fbab1cd2654cf007224a91f
jideabdqudus <jideabdqudus@gmail.com>
2020-06-23 14:48:34 +0100

    Added Commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,19 +7,29 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = "";
-
-  for (let character of str) {
-    reversed = character + reversed;
-  }
-
-  return reversed;
+  
 }
 
 module.exports = reverse;
+
+//Solution 1
 
 // function reverse(str) {
 //   const arr = str.split(""); //turns string to an array
 //   arr.reverse(); //reverses the array
 //   return arr.join(""); //joins the array to a string
 // }
+
+
+
+//Solution 2
+
+// function reverse(str) {
+//     let reversed = "";
+  
+//     for (let character of str) {
+//       reversed = character + reversed;
+//     }
+  
+//     return reversed;
+//   }
