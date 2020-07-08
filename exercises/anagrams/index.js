df4f86906e03368051c68d06c52beb1797b604c3
jideabdqudus <jideabdqudus@gmail.com>
2020-07-08 16:07:44 +0100

    Added Commit

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,14 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
 
-function anagrams(stringA, stringB){
- 
-    
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
 }
 
 module.exports = anagrams;
-
 
 // function anagrams(stringA, stringB) {
 //   const chars = {};
@@ -23,7 +42,7 @@ module.exports = anagrams;
 //   const newCharSet = {};
 
 //   for (let char of stringA.replace(/[^\w]/g, "").toLowerCase()) {
-//     chars[char] = chars[char] + 1 || 1;         
+//     chars[char] = chars[char] + 1 || 1;
 //   }
 
 //   for (let newchar of stringB.replace(/[^\w]/g, "").toLowerCase()) {
