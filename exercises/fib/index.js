0558e23739056920668c580118fc0991ce6ffc92
jideabdqudus <jideabdqudus@gmail.com>
2021-06-13 23:00:22 +0100

    +

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,46 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+
+fib = memoize(fib);
 
 module.exports = fib;
+
+// function fib(n) {
+//   let initial_array = [0, 1];
+//   for (i = 1; i < n; i++) {
+//     const sum_both = initial_array[i] + initial_array[i - 1];
+//     initial_array.push(sum_both);
+//   }
+//   return initial_array[n];
+// }
+
+// function fib(n) {
+//   const fib_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+//   const first_bracket = Math.pow((1 + Math.sqrt(5)) / 2, n);
+//   const second_bracket = Math.pow((1 - Math.sqrt(5)) / 2, n);
+//   const answer = Math.round((first_bracket - second_bracket) / Math.sqrt(5));
+//   return answer
+// }
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -3,18 +3,77 @@
 // and returns a NxN spiral matrix.
 // --- Examples
 //   matrix(2)
-//     [[1, 2],
-//     [4, 3]]
+//     [[1, 2],[4, 3]]
 //   matrix(3)
-//     [[1, 2, 3],
-//     [8, 9, 4],
-//     [7, 6, 5]]
+//     [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4],
-//     [12, 13, 14, 5],
-//     [11, 16, 15, 6],
-//     [10,  9,  8, 7]]
+//     [[1,2,3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+
+  for (i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    //Right Coulumn
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+
+    endColumn--;
+
+    //Bottom Row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+
+    endRow--;
+
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+  return results;
+}
 
 module.exports = matrix;
+
+// function matrix(n) {
+//   another_array = []
+//   empty_array = [];
+//   for (i = 1; i < n; i++) {
+//     another_array.push(i)
+//   }
+//   another_array.forEach((val) => {
+//     empty_array.push(another_array);
+//   });
+//   element_one_gone = empty_array.shift()
+//   empty_array.forEach((val)=>{
+//     const lastItem = val[val.length - 1]
+//     const new_value = lastItem ++
+//     console.log(new_value)
+//   })
+
+//   console.log(empty_array)
+// }
+// matrix(5);
